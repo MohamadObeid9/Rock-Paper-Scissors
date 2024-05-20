0af8cc7ae2bfe82c298cc6a9d18381be80c08bae
MohamadObeid9 <mohamad.obeid.2@isae.edu.lb>
2024-05-20 14:09:54 +0300

    set the game to stop if the score is equal 5 and announce the winner

--- script.js
+++ script.js
@@ -24,63 +24,90 @@ container.appendChild(finalResult);
 
 // na2sa l annoucement bl a5ir
 playGame();
-function playGame(){ 
-       rock.addEventListener("click",()=> playRound("rock",getComputerChoice()));
-       paper.addEventListener("click",()=>playRound("paper",getComputerChoice()));
-       scissor.addEventListener("click",()=> playRound("scissor",getComputerChoice()));  
-}  
+function playGame(){
+    if (humanScore < 5 || computerScore < 5) {
+            rock.addEventListener("click",()=> playRound("rock",getComputerChoice()));
+            paper.addEventListener("click",()=>playRound("paper",getComputerChoice()));
+            scissor.addEventListener("click",()=> playRound("scissor",getComputerChoice()));
+
+    }
+    else if (humanScore ===5 || computerScore ===5) {
+            if (humanScore === 5) {
+                finalResult.textContent ="you win!!";
+                endGame();
+            } else {
+                finalResult.textContent="you lose!!";
+                endGame();
+            }
+
+    }
+}
+
+function endGame() {
+    rock.disabled = true;
+    paper.disabled = true;
+    scissor.disabled = true;
+  }
 
 const  getComputerChoice =() => {
-    const Randomnum = Math.floor(Math.random()*3);  
-    if (Randomnum==0) return "rock";
-    else if (Randomnum==1)return "paper";
+    const Random  = Math.floor(Math.random()*3);
+    if (Random === 0) return "rock";
+    else if (Random === 1) return "paper";
     else return "scissor";
-}  
+}
 
 function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
+    if (humanScore === 5) {
+        finalResult.textContent="you win !!";
+        endGame();
+    }
+    else if (computerScore === 5 ) {
+        finalResult.textContent="you lose !!";
+        endGame();
+    }
+    else if (humanChoice === computerChoice) {
         display.textContent       = `HumanScore : ${humanScore} \n ComputerScore : ${computerScore}`;
         return result.textContent="Draw";
-    } 
+    }
     else if(humanChoice === "rock"){
         if(computerChoice === "scissor") {
             humanScore++;
             display.textContent       = `HumanScore : ${humanScore} \n ComputerScore : ${computerScore}`;
            return result.textContent="you win , rock beat scissor";
-           
-        }     
+
+        }
         else{
             computerScore++;
             display.textContent       = `HumanScore : ${humanScore} \n ComputerScore : ${computerScore}`;
             return result.textContent="you lose , paper beat rock";
-          }     
-        
-        }    
+          }
+
+        }
         else  if(humanChoice === "paper"){
             if(computerChoice === "rock") {
                 humanScore++;
                 display.textContent       = `HumanScore : ${humanScore} \n ComputerScore : ${computerScore}`;
                 return result.textContent="you win , paper beat rock";
-            }     
+            }
             else{
                 computerScore++;
                 display.textContent       = `HumanScore : ${humanScore} \n ComputerScore : ${computerScore}`;
                 return   result.textContent="you lose , scissor beat paper";
-            }     
-            
-        }    
-        else{
+            }
+
+        }
+        else {
             if(computerChoice === "papper") {
                 humanScore++;
                 display.textContent       = `HumanScore : ${humanScore} \n ComputerScore : ${computerScore}`;
                 return    result.textContent="you win , scissor beat paper";
-            }          
+            }
             else{
                 computerScore++;
                 display.textContent       = `HumanScore : ${humanScore} \n ComputerScore : ${computerScore}`;
                 return    result.textContent="you lose ,rock beat scissor";
-            }     
-            
-        }    
-}  
+            }
+
+        }
+}
 
